2e0ac09a333a135f71eaf815493711f222e15bcb
Eivind Ingebrigtsen <eivindingebrigtsen@gmail.com>
2010-02-07 14:00:42 +0100

    moved classes to system folder

---
+++ system/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+require('../etc/conf.php');   
+require('libs/validate_email.php'); 
+
+# Classes 
+require('site.php');
+require('log.php');
+require('i18n.php');
+require('auth.php');
+require('dbase.php');
+require('time.php');
+require('tasker.php');
+require('keywords.php');
+require('lists.php');
+# require('class/events.php');
+
+
+# Debug
+include_once 'libs/FirePHPCore/fb.php';
+FB::setEnabled(DEBUG); 
+?>
